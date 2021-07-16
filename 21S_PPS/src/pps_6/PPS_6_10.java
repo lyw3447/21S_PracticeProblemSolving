87126772cb81976ffa350ca49fc6e7ae587b2670
lyw3447 <lyw3447@naver.com>
2021-07-17 00:23:31 +0900

    6-10

---
+++ 21S_PPS/src/pps_6/PPS_6_10.java
@@ -0,0 +1,25 @@
+package pps_6;
+
+import java.util.*;
+
+public class PPS_6_10 {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		HashSet<String> set = new HashSet<String>();
+		
+		String name = "";
+		
+		for(int i = 0; i < line.length(); i++) {
+			name = "";
+			
+			for (int j = i; j < line.length(); j++) {
+				name += line.substring(j, j+1);
+				set.add(name);
+			}
+		}
+		System.out.println(set.size());
+		
+	}
+}
