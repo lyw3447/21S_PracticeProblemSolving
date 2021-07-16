bc43d947d52fed52f79facb27f3cbce3b14e166a
lyw3447 <lyw3447@naver.com>
2021-07-16 23:54:58 +0900

    .

--- 21S_PPS/src/pps_6/PPS_6_1.java
+++ 21S_PPS/src/pps_6/PPS_6_1.java
@@ -4,20 +4,7 @@ import java.util.*;
 public class PPS_6_1 {
 
 	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		
-		Set<Integer> set = new TreeSet<Integer>();
-		while(n-->0) {
-			set.add(sc.nextInt());
-		}
-		
-		Iterator it = set.iterator();
-		
-		while (it.hasNext()) {
-			System.out.println(it.next());
-		}
 	}
 	
 	
