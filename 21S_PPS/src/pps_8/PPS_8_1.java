24b92ae570e728b366bef459ef80075d4fe31f00
lyw3447 <lyw3447@naver.com>
2021-07-20 23:26:21 +0900

    7/19 pps

---
+++ 21S_PPS/src/pps_8/PPS_8_1.java
@@ -0,0 +1,34 @@
+package pps_8;
+import java.util.*;
+
+public class PPS_8_1 {
+	public static void main(String[] args) throws Exception{
+		int cookie[] = {1, 1, 2, 3}; //3
+		int leftSum, leftIdx;
+        int rightSum, rightIdx;
+        int answer = 0;
+        
+        for(int i = 0; i < cookie.length-1; i++) {   
+            leftSum =cookie[i];
+            leftIdx = i;
+            rightSum =cookie[i+1];
+            rightIdx = i+1; 
+            
+            while(true) {
+                if(leftSum == rightSum && answer < leftSum) {
+                    answer = leftSum;
+                }
+                if(leftIdx > 0 && leftSum <= rightSum) {
+                    leftIdx--;
+                    leftSum += cookie[leftIdx];
+                }else if(rightIdx < cookie.length-1 && leftSum >= rightSum) {
+                    rightIdx++;
+                    rightSum += cookie[rightIdx];
+                }else {
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+	}
+}
