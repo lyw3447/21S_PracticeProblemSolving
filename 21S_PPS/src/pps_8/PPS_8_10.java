28eb8fbb9b62b47faa6c173dc596e580b20b3829
lyw3447 <lyw3447@naver.com>
2021-07-22 00:25:33 +0900

    7/20 (4)

--- 21S_PPS/src/pps_8/PPS_8_10.java
+++ 21S_PPS/src/pps_8/PPS_8_10.java
@@ -1,10 +1,28 @@
 package pps_8;
 import java.io.*;
+import java.util.StringTokenizer;
 
 //https://www.acmicpc.net/problem/2783
 
 public class PPS_8_10 {
 	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		float min = Float.parseFloat(st.nextToken()) / Float.parseFloat(st.nextToken());
+		
+		int n = Integer.parseInt(br.readLine());
+	
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			float tmp = Float.parseFloat(st.nextToken()) / Float.parseFloat(st.nextToken());
+			
+			if (tmp < min) 
+				min = tmp;
+			
+		}
+		
+		System.out.println(String.format("%.2f", min*1000));
 		
 	}
 }
