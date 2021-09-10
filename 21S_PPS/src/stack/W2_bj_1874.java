d4fb9b08c4b62a58b8500e5d45d2ac6d877e1473
lyw3447 <lyw3447@naver.com>
2021-09-10 17:21:19 +0900

    week2 - (3)

---
+++ 21S_PPS/src/stack/W2_bj_1874.java
@@ -0,0 +1,24 @@
+package stack;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/1874
+
+public class W2_bj_1874 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int n = s.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			int num = s.nextInt();
+			
+			
+		}
+		
+		
+
+	}
+
+}
