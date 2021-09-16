b025663d265d061a7caf1348f4a6204c26e47aea
lyw3447 <lyw3447@naver.com>
2021-09-17 00:50:22 +0900

    Week3 completed!!!!

--- 21S_PPS/src/stack/W3_bj_9012.java
+++ 21S_PPS/src/stack/W3_bj_9012.java
@@ -1,6 +1,7 @@
 package stack;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 //https://www.acmicpc.net/problem/9012
 public class W3_bj_9012 {
@@ -8,7 +9,28 @@ public class W3_bj_9012 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
+		int T = s.nextInt();
 		
+		for (int i=0; i<T; i++) {
+			String line = s.next();
+			Stack<Integer> stack = new Stack<Integer>();
+			
+			for (int j=0; j<line.length(); j++) {
+				if (line.charAt(j) == '(') stack.push(1);
+			
+				else {
+					if (!stack.empty()) stack.pop();
+					else {
+						System.out.println("NO");
+						break;
+					}
+				}
+				if (j == line.length()-1 && stack.empty())
+					System.out.println("YES");
+				else if(j == line.length()-1)
+					System.out.println("NO");
+			}
+		}
 
 	}
 
